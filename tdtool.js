c144e4963862b84337246f9634867064bdad4ea7
Patrik Olsson <patrik.olsson@x-firm.com>
2013-06-29 22:23:50 +0200

    Made some changes to speed up things and also to separate the control code from tdtool code

--- tdtool.js
+++ tdtool.js
@@ -22,8 +22,8 @@ function tdtool () {
 	}
  	arguments.callee._singletonInstance = this;
 
- 	this.readTellstickList = function(callback, units){
-	  var units = units;
+	this.readUnitsValue = function(callback){
+	  var values = [];
 
 	  // Not yet implemented
 	  // Read data from the tdtool -l command and format it as the unit data object
@@ -32,62 +32,55 @@ function tdtool () {
 	    child = exec(str, function (error, stdout, stderr) {
 	      if (error !== null) {
 	        console.log('exec error: ' + error);
+	        callback();
 	      }else{
-	        var index = 0;
+	        var index = 1;
 	        var strings = stdout.split('\n');
 
 	        for (var i = 1; i<strings.length; i++) {
-	          if(strings[i-1] != ""){
-	            if((index+1) < units.length){
+	          // String not empty
+	          if(strings[i-1] != ""){ 
 	              var parts = strings[i-1].split('\t'); 
 
-	              if(parts[2] == "ON"){
-	                units[index].currentValue = true;
-	                index++;
-	              }else if(parts[2] == "OFF"){
-	                units[index].currentValue = false;
-	                index++;
-	              }
-	            }
+	              // Check that row has right syntax
+	              if(parts.length >= 3){ 
+
+	              	// Check if value is ok
+	              	var value = new Object();
+					value.id = index;
+		            value.name = parts[1];
+		            value.currentValue = false;
+					value.currentDimValue = 255;
+
+	              	if(parts[2] == "ON"){
+	              	  	value.currentValue = true;
+						value.currentDimValue = 255;
+		            }else if(parts[2] == "OFF"){
+	              	  	value.currentValue = false;
+						value.currentDimValue = 255;
+		            }else{
+		            	var dim = parts[2].split(':');
+		            	if(dim.length >= 1){
+		            		value.currentValue = true;
+							value.currentDimValue = dim[1];
+		            	}
+		            }
+
+		            values.push(value);
+
+		            index++;
+		          }  
 	          } 
 	        }
-
-	        callback(units);
+	        callback(values);
 	      }
-	    });
+		});
 	  }else{
-	    callback(units);
+	    callback(values);
 	  }
 	}
 
-	this.setTellstickUnitGroupValue = function(callback, group, value){
-		var group = group;
-
-		var str = "tdtool ";
-
-		for (var i = 0; i < group.length; i++) {
-			var unit = group[i].id
-			if(value){
-	     	  str = str + " --on " + unit;
-		    }else{
-		      str = str + " --off " + unit;
-		    }
-
-		};
-
-		//console.log(str);
-	    
-	    child = exec(str, function (error, stdout, stderr) {
-	      if (error !== null) {
-	        console.log('exec error: ' + error);
-	      }else{
-	      	console.log(stdout);
-	        callback();
-	      }
-	    });
-	}
-
-	this.setTellstickUnitValue = function(callback, unit, value, units){
+	this.setTellstickUnitValue = function(callback, unitId, value, id, units){
 	  var units = units;
 	  // Not yet implemented
 	  // Set device state with the tdtool --on 'unit' command
@@ -95,25 +88,59 @@ function tdtool () {
 	    var str = "tdtool ";
 
 	    if(value){
-	      str = str + "--on " + unit;
+	      str = str + "--on " + unitId;
 	    }else{
-	      str = str + "--off " + unit;
+	      str = str + "--off " + unitId;
 	    }
 
 	    child = exec(str, function (error, stdout, stderr) {
 	      if (error !== null) {
 	        console.log('exec error: ' + error);
+	        callback();
 	      }else{
 	        // Should be removed when the list of devices is
 	        // generated 
-	        units[unit-1].currentValue = value;
+	        units[id].currentValue = value;
 	        //
 	        callback();
 	      }
 	    });
 	  }else{
 
-	    units[unit-1].currentValue = value;
+	    units[id].currentValue = value;
+
+	    callback();
+	  }
+	}
+
+	this.setTellstickUnitDimValue = function(callback, unitId, value, id, units){
+	  var units = units;
+	  // Not yet implemented
+	  // Set device state with the tdtool --on 'unit' command
+	  if(!debug){
+	    var str = "tdtool ";
+
+
+	     str = str + "-v " + value + " -d " + unitId;
+
+
+	    child = exec(str, function (error, stdout, stderr) {
+	      if (error !== null) {
+	        console.log('exec error: ' + error);
+	        callback();
+	      }else{
+	        // Should be removed when the list of devices is
+	        // generated 
+	        units[id].currentValue = true;
+	        units[id].currentDimValue = value;
+	        //
+	        callback();
+	      }
+	    });
+	  }else{
+	  	
+	  	units[id].currentValue = true;
+	    units[id].currentDimValue = value;
 
 	    callback();
 	  }
